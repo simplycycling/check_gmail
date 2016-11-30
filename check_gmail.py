80a6615db9deecddc44e5ff6afffdf794adbf914
Roger Sherman <rsherman@viddler.com>
2016-11-30 17:23:51 +1100

    Added function to trigger Gnome notification if there is new email.

--- check_gmail.py
+++ check_gmail.py
@@ -1,10 +1,19 @@
 import imaplib
 
 
+def mail_time():
+    import gi
+    gi.require_version('Notify', '0.7')
+    from gi.repository import Notify
+    Notify.init("Mail Check")
+    new_mail = Notify.Notification.new("You have mail.")
+    new_mail.show()
+
 mailcall = imaplib.IMAP4_SSL('imap.gmail.com', '993')
-mailcall.login('slammingrooves@gmail.com', 'dnazuedrhfqrnkjx')
+mailcall.login('@gmail.com', '')
 mailcall.select()
 a = len(mailcall.search(None, 'UnSeen')[1][0].split())
-print(a)
+if a > 0:
+    mail_time()
 
 # TODO call the username and password from a gpg encrypted file
